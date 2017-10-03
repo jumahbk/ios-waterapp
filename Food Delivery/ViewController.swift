bb4211a92ed2ced03b7cd9a40177aef991f81fda
Bander Jumah <bj@Banders-MacBook-Pro.local>
2017-10-03 21:14:23 +0300

    init

---
+++ Food Delivery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Delivery
+//
+//  Created by Bander Jumah on 10/3/17.
+//  Copyright © 2017 Paragon Saudi Arabia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
