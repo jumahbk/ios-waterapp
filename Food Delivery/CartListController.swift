99a8621469bcc882fb22d3b7d076d13f76267dde
Bander <bander@Banders-iMac.local>
2017-10-13 03:43:05 -0700

    Products

--- Food Delivery/CartListController.swift
+++ Food Delivery/CartListController.swift
@@ -10,7 +10,7 @@ import QuartzCore
 class CartListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
