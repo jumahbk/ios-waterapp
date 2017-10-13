7d589b621e545b75ea43638eca076cd94c79581c
Bander <bander@Banders-iMac.local>
2017-10-13 08:49:07 -0700

    Products

---
+++ Food Delivery/StatusViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatusViewTableViewCell.swift
+//  Food Delivery
+//
+//  Created by Bander on 13/10/2017.
+//  Copyright © 2017 Paragon Saudi Arabia. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
